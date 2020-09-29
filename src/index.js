75eef32323496c9d1e964d6f90bd53b516fd657e
Evgeniy Marinovich <zmarinovich@mail.ru>
2020-09-29 09:35:30 +0300

    feat: solve reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let number = ("" + n).split("").reverse().join("");
+    if (number[number.length - 1] === "-") {
+        number = number.slice(0, number.length - 1);
+    }
+    return +number;
+
+};
